16d7d0686e77630f754970750cb0cba058a690b9
Marvin Deuschle <marvdeus@gmail.com>
2021-05-28 17:29:53 +0200

    adding set_bits function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cmath>
+
+int players[2]{0, 1};
+int start = 0;
+
+int set_bits(const int bits[], const size_t size) {
+    int result = 0;
+    for (int i = 0; i < size; i++) {
+        result |= 1 << bits[i];
+    }
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
