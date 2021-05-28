092af9c7c7ed48f735e09f1a94cec669f3591524
Marvin Deuschle <marvdeus@gmail.com>
2021-05-28 17:42:33 +0200

    adding to_board function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 int players[2]{0, 1};
 int start = 0;
@@ -16,7 +17,27 @@ int set_bit(int power) {
     return 1 << power;
 }
 
+std::string to_board(int state) {
+    std::string result = "+-+-+-+\n";
+    for (int cell = 0; cell < 9; cell++) {
+        if ((state & set_bit(cell)) != 0) {
+            result += "|X";
+        } else if ((state & set_bit(cell + 9)) != 0) {
+            result += "|O";
+        } else {
+            result += "| ";
+        }
+        if ((cell + 1) % 3 == 0) {
+            result += "|\n+-+-+-+\n";
+        }
+    }
+    return result;
+}
+
 int main() {
+    int bits[]{0, 2, 3, 5, 7, 9 + 1, 9 + 4, 9 + 6};
+    int state = set_bits(bits, 8);
+    std::cout << to_board(state);
     return 0;
 }
 
